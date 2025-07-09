8b1e2e771444f7d110b102148fe3071fa24d9598
LoneExpert <aryanshubbu171@gmail.com>
2025-07-09 13:55:32 +0530

    Some changes done

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const nextConfig = {
     unoptimized: true,
   },
   basePath: '/Pokemon-Explorer',       // ✅ Replace with your actual repo name
-  assetPrefix: '/Pokemon-Explorer',    // ✅ Same here for assets like images & CSS
 };
 
 export default nextConfig;
